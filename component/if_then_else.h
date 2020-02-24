ba7855a02e67365534be50df0aa5e8e374c02895
lgq <1332728814@qq.com>
2020-02-24 16:56:30 +0800

    update if_then_else.h

--- component/if_then_else.h
+++ component/if_then_else.h
@@ -1,16 +1,16 @@
 #pragma once
 template<bool BT_,typename T1,typename T2>
-class IfThenElese;
+class IfThenElse;
 
 template<typename T1,typename T2>
-class IfThenElese<true, T1, T2>
+class IfThenElse<true, T1, T2>
 {
 public:
 	typedef T1 type;
 };
 
 template<typename T1,typename T2>
-class IfThenElese<false, T1, T2>
+class IfThenElse<false, T1, T2>
 {
 public:
 	typedef T2 type;
